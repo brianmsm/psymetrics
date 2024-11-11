7a7e8784c2c5634fc5f292fcef06d8d9261cbb7c
Brian Norman Peña Calero <brianmsm@gmail.com>
2024-11-12 00:10:38 +0100

    docs: add save_table function for exporting tables
    Add the `save_table` function to export data frames as Word 
    documents (.docx) using the `flextable` and `officer` packages. 
    Include parameters for table data, file path, template, and 
    decimal formatting. Update documentation and NAMESPACE to 
    reflect the new functionality. This enhances the package's 
    capabilities for table presentation in reports.

--- R/save_table.R
+++ R/save_table.R
@@ -1,3 +1,44 @@
+#' Save a Table to a Specified Format
+#'
+#' @description
+#' `save_table()` exports a table to various formats, with current support for Word documents (.docx).
+#' The table is formatted using a specified or default template and can be customized with different font styles,
+#' sizes, and alignment options.
+#'
+#' @param table_data A data frame containing the table data to be exported.
+#' @param path A string specifying the file path where the table should be saved. The file extension determines
+#'   the format (currently only .docx is supported).
+#' @param template A string specifying the path to a Word template (.docx) to be used. If NULL, a default
+#'   template provided by the package is used.
+#' @param digits An integer indicating the number of decimal places to use when formatting numeric columns.
+#'   Defaults to 3.
+#' @param ... Additional arguments passed to `flextable::tt` when exporting to Word.
+#'
+#' @details
+#' This function checks the file extension to determine the export format. Currently, only Word documents
+#' (.docx) are supported. The function formats the table using the `flextable` package and exports it
+#' using the `officer` package. Users can provide a custom Word template or rely on the package's default
+#' template.
+#'
+#' If the `path` has a .docx extension, the table will be saved as a Word document. The default template
+#' aligns the table to APA style, and the font is set to Arial with a font size of 10.
+#'
+#' @examples
+#' \dontrun{
+#' library(psymetrics)
+#'
+#' table_data <- data.frame(
+#'   Model = c("fit1", "fit2"),
+#'   NOBS = c(301, 301),
+#'   Chi2 = c(87.13, 24.37),
+#'   DF = c(24, 8),
+#'   pvalue = c(0.000, 0.002)
+#' )
+#'
+#' save_table(table_data, path = "model_fit.docx")
+#' }
+#'
+#' @export
 save_table <- function(table_data, path, template = NULL, digits = 3, ...) {
   # Check if required packages are installed
   rlang::check_installed("tools")
