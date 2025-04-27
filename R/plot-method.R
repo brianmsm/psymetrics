cdb9cc62fade4ee5c030e8717d9590bc1f2d3171
Brian Norman Peña Calero <brianmsm@gmail.com>
2025-04-27 14:58:00 +0200

    docs: add detailed generic plot function documentation
    Add comprehensive Rd documentation for the generic plot function,
    including current support for lavaan models and planned extensions
    for psych and mirt packages. Remove redundant plot generic function
    definition and update roxygen comments for clarity and consistency.

--- R/plot-method.R
+++ R/plot-method.R
@@ -1,11 +1,14 @@
 #' Generic Plot Function for Model Visualizations
 #'
+#' @name plot-methods
+#'
 #' @description
 #' `plot()` is a generic function designed for visualizing various aspects of
 #' statistical models. It provides a unified interface for generating plots that
 #' vary depending on the model type and the specific method implemented for that
 #' model class.
 #'
+#' @details
 #' Current available method:
 #' - **Confirmatory Factor Analysis**: [plot.lavaan()] for plotting factor loadings and other metrics for models from the `lavaan` package.
 #'
@@ -13,10 +16,9 @@
 #' - **Exploratory Factor Analysis**: Models from the `psych` package.
 #' - **Item Response Theory (IRT)**: Models from the `mirt` package.
 #'
-#' @param x A model object. Specific methods exist for different model types.
-#' @param ... Additional arguments passed to the plot method.
 #' @return A plot object, usually a ggplot, specific to the model type.
+#' @seealso
+#'  [plot.lavaan()] for lavaan objects
 #' @keywords internal
-plot <- function(x, ...) {
-  UseMethod("plot")
-}
+#'
+NULL
